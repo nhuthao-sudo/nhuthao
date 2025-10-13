e8ecf6af984c2c226571f5c1ffd98b6dda543872
nhuthao-sudo <hao123601@gmail.com>
2025-10-13 08:57:57 +0700

    TUAN_2

--- TUAN_2/my-clothes-shop/src/components/Header.jsx
+++ TUAN_2/my-clothes-shop/src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="bg-blue-600 text-white py-3 px-4 rounded-b-2xl">
-      <h1 className="text-xl font-bold">💻 Computer Store</h1>
+      <h1 className="text-xl font-bold">💻 Clothes Store</h1>
     </header>
   );
 }
